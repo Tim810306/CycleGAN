83d4e327426845a016cdb290979a9244c168d6a3
Tim Cheng <timazoey@gmail.com>
2018-03-01 17:20:32 +0800

    update convert.py

--- convert.py
+++ convert.py
@@ -1,11 +1,11 @@
 import cv2
 print(cv2.__version__)
-vidcap = cv2.VideoCapture('eiffel.mp4')
+vidcap = cv2.VideoCapture('xxx.mp4')
 success,image = vidcap.read()
 count = 0
 success = True
 while success:
-  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
+  cv2.imwrite("%d.jpg" % count, image)     # save frame as JPEG file
   success,image = vidcap.read()
   print 'Read a new frame: ', success
   count += 1
